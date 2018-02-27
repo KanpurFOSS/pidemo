1cd3ee3c00dd439f5c5e37e609c4ebc07cf52671
Vivek Kumar Kanaujia <vivek.aris@gmail.com>
2018-02-27 18:17:40 +0530

    Create Blinker.py
    connect led at pin 32 on pi3

---
+++ Blinker.py
@@ -0,0 +1,24 @@
+# External module imports
+import RPi.GPIO as GPIO
+import time
+
+# Pin Definitons:
+ledPin = 12 # Broadcom pin 32 (P1 pin 16)
+
+
+# Pin Setup:
+GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+GPIO.setup(ledPin, GPIO.OUT) # LED pin set as output
+
+# Initial state for LEDs:
+GPIO.output(ledPin, GPIO.LOW)
+
+print("Here we go! Press CTRL+C to exit")
+try:
+    while 1:
+        GPIO.output(ledPin, GPIO.HIGH)
+        time.sleep(0.075)
+        GPIO.output(ledPin, GPIO.LOW)
+        time.sleep(0.075)
+except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+    GPIO.cleanup() # cleanup all GPIO
